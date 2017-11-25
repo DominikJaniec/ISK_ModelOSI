132d64f2b97685e28b6a291df53f9cf940b986cf
Piotr Hejmej <piotrek10h@gmail.com>
2017-11-25 20:55:42 +0100

    add session-layer component

---
+++ osi/src/app/osi-container/session-layer/session-layer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-session-layer',
+  templateUrl: './session-layer.component.html',
+  styleUrls: ['./session-layer.component.css']
+})
+export class SessionLayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
