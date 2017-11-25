fcf601a4cb554c92e1a2ce874cbe289ea01c3431
Piotr Hejmej <piotrek10h@gmail.com>
2017-11-25 20:56:23 +0100

    add components to app,module

--- osi/src/app/app.module.ts
+++ osi/src/app/app.module.ts
@@ -7,6 +7,12 @@ import { EnpointsComponent } from './enpoints/enpoints.component';
 import { FileLoaderComponent } from './file-loader/file-loader.component';
 import { OsiContainerComponent } from './osi-container/osi-container.component';
 import { ApplicationLayerComponent } from './osi-container/application-layer/application-layer.component';
+import { PresentationLayerComponent } from './osi-container/presentation-layer/presentation-layer.component';
+import { SessionLayerComponent } from './osi-container/session-layer/session-layer.component';
+import { TransportLayerComponent } from './osi-container/transport-layer/transport-layer.component';
+import { NetworkLayerComponent } from './osi-container/network-layer/network-layer.component';
+import { DatalinkLayerComponent } from './osi-container/datalink-layer/datalink-layer.component';
+import { PhysicalLayerComponent } from './osi-container/physical-layer/physical-layer.component';
 
 
 @NgModule({
@@ -15,7 +21,13 @@ import { ApplicationLayerComponent } from './osi-container/application-layer/app
     EnpointsComponent,
     FileLoaderComponent,
     OsiContainerComponent,
-    ApplicationLayerComponent
+    ApplicationLayerComponent,
+    PresentationLayerComponent,
+    SessionLayerComponent,
+    TransportLayerComponent,
+    NetworkLayerComponent,
+    DatalinkLayerComponent,
+    PhysicalLayerComponent
   ],
   imports: [
     BrowserModule
