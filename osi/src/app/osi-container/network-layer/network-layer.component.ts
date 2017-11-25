507908f2539d8f0d4af35f94182c80e0f9ab35a5
Piotr Hejmej <piotrek10h@gmail.com>
2017-11-25 20:55:17 +0100

    add network-layer component

---
+++ osi/src/app/osi-container/network-layer/network-layer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-network-layer',
+  templateUrl: './network-layer.component.html',
+  styleUrls: ['./network-layer.component.css']
+})
+export class NetworkLayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
