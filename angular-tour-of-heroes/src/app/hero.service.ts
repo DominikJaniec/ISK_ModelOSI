e281107a4221db8536cdcfdd843674af25b58987
Dominik Janiec <dominikjaniec@gmail.com>
2017-11-25 12:09:36 +0100

    Tutorial 6. Services - Messages log implemented

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -4,12 +4,16 @@ import { of } from 'rxjs/observable/of';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessagesService } from './messages.service';
+import { __core_private_testing_placeholder__ } from '@angular/core/testing';
 
 @Injectable()
 export class HeroService {
-  constructor() {}
+  constructor(private messageService: MessagesService) {}
 
   getHeroes(): Observable<Hero[]> {
+    // TODO: Send the message _after_ fetching the heroes.
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
