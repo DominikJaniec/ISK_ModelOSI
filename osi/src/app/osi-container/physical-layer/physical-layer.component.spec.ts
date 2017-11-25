dc9017fdd212e6aac7c53932becaeeea576eeebb
Piotr Hejmej <piotrek10h@gmail.com>
2017-11-25 20:55:24 +0100

    add physical-layer component

---
+++ osi/src/app/osi-container/physical-layer/physical-layer.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PhysicalLayerComponent } from './physical-layer.component';
+
+describe('PhysicalLayerComponent', () => {
+  let component: PhysicalLayerComponent;
+  let fixture: ComponentFixture<PhysicalLayerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PhysicalLayerComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PhysicalLayerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ osi/src/app/osi-container/physical-layer/physical-layer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-physical-layer',
+  templateUrl: './physical-layer.component.html',
+  styleUrls: ['./physical-layer.component.css']
+})
+export class PhysicalLayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
