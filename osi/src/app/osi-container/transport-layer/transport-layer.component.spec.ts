9d5811efff882c2e0c5a11e2a3f4f3957c7e43f7
Piotr Hejmej <piotrek10h@gmail.com>
2017-11-25 20:55:52 +0100

    add transport-layer component

---
+++ osi/src/app/osi-container/transport-layer/transport-layer.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TransportLayerComponent } from './transport-layer.component';
+
+describe('TransportLayerComponent', () => {
+  let component: TransportLayerComponent;
+  let fixture: ComponentFixture<TransportLayerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TransportLayerComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TransportLayerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ osi/src/app/osi-container/transport-layer/transport-layer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transport-layer',
+  templateUrl: './transport-layer.component.html',
+  styleUrls: ['./transport-layer.component.css']
+})
+export class TransportLayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
