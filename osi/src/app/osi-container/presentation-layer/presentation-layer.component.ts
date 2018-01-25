a284b413c9161c59030027c370ae82770faccbfc
Piotr Hejmej <piotrek10h@gmail.com>
2018-01-25 23:13:19 +0100

    add crypto-js and add ciphers to presentation layer

--- osi/src/app/osi-container/presentation-layer/presentation-layer.component.ts
+++ osi/src/app/osi-container/presentation-layer/presentation-layer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
+import * as CryptoJS from 'crypto-js';
 
 import {
   OrchestratorService,
@@ -18,16 +19,32 @@ export class PresentationLayerComponent implements OnDestroy, LayerContent {
   private subscription: Subscription;
   private direction: Direction;
   data: LayerData;
-  dat : string;
-
+  dat: string;
+  ciphers: cipherAlgorithms[];
+  selectedCipher: cipherAlgorithms;
+  cipherKey: string = "";
+  encryptedData: string = "";
+  deshifre: string = "";
 
   constructor(
     private readonly orchestrator: OrchestratorService,
     private readonly translate: TranslateService
   ) {
+    this.ciphers = [
+      { name: "", id: 0 },
+      { name: "AES", id: 1 },
+      { name: "DES", id: 2 },
+      { name: "Triple DES", id: 3 },
+      { name: "Rabbit", id: 4 },
+      { name: "Rabbit Legacy", id: 5 },
+      { name: "RC4", id: 6 },
+      { name: "RC4Drop", id: 7 },
+    ]
   }
 
   initialize(direction: Direction) {
+    
+
     this.subscription = registerDummyRepeater(
       {
         kind: LayerKind.Presentation,
@@ -50,14 +67,52 @@ export class PresentationLayerComponent implements OnDestroy, LayerContent {
   }
   pushData(data: any)
   {
-    data += "GGWP";
     this.orchestrator.ready({
       kind: LayerKind.Presentation,
       direction: this.direction
     }, { blocks: [this.contentToData(data)]} );
 
   }
-  
+
+  onCipherSelectChange(value: number)
+  {
+    if (value > 0)
+      this.selectedCipher = this.ciphers.find(r => r.id == value);
+    else
+      this.selectedCipher = null;
+  }
+
+  EncryptData() {
+    var data = this.dat.toString();
+    var cipherKey = this.cipherKey.toString();
+
+    switch (this.selectedCipher.id)
+    {
+      case 1: {
+        this.encryptedData = CryptoJS.AES.encrypt(data, cipherKey).toString();        
+      }
+      case 2: {
+        this.encryptedData = CryptoJS.DES.encrypt(data, cipherKey).toString();
+      }
+      case 3: {
+        this.encryptedData = CryptoJS.TripleDES.encrypt(data, cipherKey).toString();
+      }
+      case 4: {
+        this.encryptedData = CryptoJS.Rabbit.encrypt(data, cipherKey).toString();
+      }
+      case 5: {
+        this.encryptedData = CryptoJS.RabbitLegacy.encrypt(data, cipherKey).toString();
+      }
+      case 6: {
+        this.encryptedData = CryptoJS.RC4.encrypt(data, cipherKey).toString();
+      }
+      case 7: {
+        this.encryptedData = CryptoJS.RC4Drop.encrypt(data, cipherKey).toString();
+      }
+    }
+  }
+
+
 contentToData(data: string): DataBlock {
   // TODO: Load data as bytes, use encoding?
   //this.data = ;
@@ -71,4 +126,9 @@ contentToData(data: string): DataBlock {
   getModeName() {
     return this.translate.direction(this.direction);
   }
+}
+
+class cipherAlgorithms {
+  name: string;
+  id: number;
 }
