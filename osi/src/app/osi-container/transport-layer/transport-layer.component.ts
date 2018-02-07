b7dc9e6ec9dab2060639ecbd264c0b541908dae0
Piotr Hejmej <piotrek10h@gmail.com>
2018-02-07 23:40:55 +0100

    add some basic description to session layer, as we are not realy able to seriously implement sesion functionality (too much effort for too little benefits)
    add core functionality of transport layer:
    * steal string to bit methods
    * add method for finding parity bit in bytes saved in string
    * add small method to shorten long strings into readable form
    
    ((Really bad and long commit))

--- osi/src/app/osi-container/transport-layer/transport-layer.component.ts
+++ osi/src/app/osi-container/transport-layer/transport-layer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
+import { Format, Symbol } from '../../domain/symbol';
 
 import {
   OrchestratorService,
@@ -17,6 +18,10 @@ import { LayerContent } from '../layer-content';
 export class TransportLayerComponent implements OnDestroy, LayerContent {
   private subscription: Subscription;
   @Input() direction: Direction;
+  data: LayerData;
+  dat: string;
+  dateByteArray: string;
+  parityBit: number = 0;
 
   constructor(
     private readonly orchestrator: OrchestratorService,
@@ -32,6 +37,63 @@ export class TransportLayerComponent implements OnDestroy, LayerContent {
       this.orchestrator
     );
     this.direction = direction;
+
+    this.orchestrator.registerLayer({
+      kind: LayerKind.Transport,
+      direction: direction
+    }).layerData.subscribe(data => {
+      this.data = data;
+      this.dat = data.blocks[0].bytes[0];
+      this.dateByteArray = this.process(this.dat);
+      this.parityBit = this.getParityBit(this.dateByteArray);
+    });
+  }
+
+  process(content: string): string {
+    const bytes = this.toBytes(content);
+    var currentSymbols = "";
+
+    for (let i = 0; i < bytes.length; ++i) {
+      currentSymbols += Symbol.fromByte(bytes[i], Format.Binary);
+
+    }
+    return currentSymbols;
+  }
+
+  private toBytes(data: string): number[] {
+    return data.split('').map(c => c.charCodeAt(0));
+  }
+
+  getBatchedstring(data: string): string {
+    var processedData = "";
+    if (data.length > 50) {
+      let batches: string[] = [];
+      for (let i = 0; i < data.length; i++)
+      {
+        batches.push(data.slice(i, i + 25));
+        i += 25;
+      }
+      processedData = batches[0] + "..." + batches[batches.length - 1];
+    }
+    else
+      processedData = data;
+
+    return processedData;
+  }
+
+  getParityBit(data: string): number
+  {
+    var parityCounter = 0;
+
+    for (let character of data)
+    {
+      if (character === '1')
+        parityCounter++;
+    }
+    if (parityCounter % 2 == 0)
+      return 0;
+    else
+      return 1;
   }
 
   ngOnDestroy() {
